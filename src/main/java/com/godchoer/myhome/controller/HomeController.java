795a34b348e83a30686309585304105c968d8773
userHyoin <chiw9123@gmail.com>
2023-06-15 12:21:22 +0900

    layout 구성

--- src/main/java/com/godchoer/myhome/controller/HomeController.java
+++ src/main/java/com/godchoer/myhome/controller/HomeController.java
@@ -1,4 +1,13 @@
 package com.godchoer.myhome.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping
+    public String index(){
+        return "index";
+    }
 }
